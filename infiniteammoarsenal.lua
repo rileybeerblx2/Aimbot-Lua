23e10fec6bf1b5cacf18deb851e8635425e15a9a
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-27 17:56:24 +0800

    Create infiniteammoarsenal.lua

---
+++ infiniteammoarsenal.lua
@@ -0,0 +1,5 @@
+while wait() do
+        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+        game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
+    end
+end)
