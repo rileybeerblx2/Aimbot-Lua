001857d3aa6a08054ec74ee163c3a107e374cbe8
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-27 18:51:53 +0800

    Create dahoodaimlockscript.lua

---
+++ dahoodaimlockscript.lua
@@ -0,0 +1,9 @@
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+Notify({
+Description = "Riley Aim Lock";
+Title = "Enjoy Using The Aimlock!";
+Duration = 15;
+});
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rileybeerblx2/Aimbot-Lua/main/dahoodaimlock.lua"))();
